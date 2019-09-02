ab8c1244378d79d806671df379ddc77f87565c76
Seth Daetwiler <daetwis@wwu.edu>
2019-09-02 12:07:13 -0700

    tested -> functional : date data is hardcoded : add error corrections with file output for waitlists or full classes

--- WwuClassReg.py
+++ WwuClassReg.py
@@ -2,13 +2,19 @@
 
 import sys
 import os
+import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# TODO: read file for input
-username = 'daetwis@wwu.edu'
-password = 'slip-3321'
+f = open("input.txt", "r")
+contents = f.readlines()
 
+
+# read file for user id input
+username = contents[0][10:]
+password = contents[1][10:]
+
+#open chrome browser
 browser = webdriver.Chrome('./chromedriver')
 browser.get('https://websso.wwu.edu/cas/login?service=https://mywestern.wwu.edu/mywestern/Login')
 
@@ -16,34 +22,47 @@ browser.get('https://websso.wwu.edu/cas/login?service=https://mywestern.wwu.edu/
 usrInput = browser.find_element_by_id('username')
 usrInput.send_keys(username)
 
-#find and send keys to password input with contents of password
+#find and send keys to password input with contents of password and submit page
 passInput = browser.find_element_by_id('password')
-passInput.send_keys(password)
+passInput.send_keys(password+Keys.ENTER)
 
 #enter
-passInput.send_keys(Keys.ENTER)
 
-# #nav to web4U
-# browser.get('https://admin.wwu.edu/pls/wwis/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu')
+#registration status
+browser.get('https://admin.wwu.edu/pls/wwis/bwskrsta.P_RegsStatusDisp')
 
-# #select student
-# studentBtn = browser.find_element_by_link_text('Student')
-# studentBtn.click()
-
-# #select registration
-# regBtn = browser.find_element_by_link_text('Registration')
-# regBtn.click()
-
-# nav to term selection
-browser.get('https://admin.wwu.edu/pls/wwis/bwskfreg.P_AltPin')
 # TODO: put in date processing to select appropriate term reletive to date of execution
+
 select = browser.find_element_by_id('term_id')
 for option in select.find_elements_by_tag_name('option'):
-    if option.text == 'Fall 2019':
+    if 'Fall 2019' in option.text:
         option.click() # select() in earlier versions of webdriver
         break
 
+#submit term selection
 browser.find_element_by_xpath("//input[@value='Submit']").click()
+
+#get regDate
+regDate = browser.find_element_by_xpath("//span[contains(.,'If eligible')]").text
+
+#substring regDate for key values
+regDate = regDate[37:]
+regTime = regDate[-8:]
+regDate = regDate[:10]
+
+
+# nav to crn picker
+browser.get('https://admin.wwu.edu/pls/wwis/bwskfreg.P_AltPin')
+
+# in crn picker, loop through input for coresponding crn input
+for i in range(1,9):
+        crnId = ("crn_id%i" % i)
+        crnInput = browser.find_element_by_id(crnId)
+        
+        crnInput.send_keys(contents[1+i][7:])
+
+
+
 
 
 
